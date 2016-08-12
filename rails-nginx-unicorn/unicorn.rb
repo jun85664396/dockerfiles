2b93c2b77396572b6f05d08ebb12eabd97cb6091
jun85664396 <jun85664396@gmail.com>
2016-08-12 22:26:56 +0900

    -

--- rails-nginx-unicorn/unicorn.rb
+++ rails-nginx-unicorn/unicorn.rb
@@ -10,3 +10,12 @@ stdout_path "#{app_dir}/log/unicorn.stdout.log"
 worker_processes (ENV["UNICORN_WOKRER"] || 4).to_i
 listen "/tmp/unicorn.sock", :backlog => 64
 timeout 30
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['DB_POOL'] || 5
+    ActiveRecord::Base.establish_connection(config)
+  end 
+end
